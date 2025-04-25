3ea7ef225166deabe81008f34b62f90d4521b0f7
Vina del Mar <vina@duoc.acad>
2025-04-24 20:40:47 -0400

    actualizacion

---
+++ Gestion-pagos-factura/src/main/java/com/edutech/Gestion_pagos_factura/GestionPagosFacturaApplication.java
@@ -0,0 +1,13 @@
+package com.edutech.Gestion_pagos_factura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPagosFacturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPagosFacturaApplication.class, args);
+	}
+
+}
---
+++ Gestion-pagos-factura/src/test/java/com/edutech/Gestion_pagos_factura/GestionPagosFacturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.Gestion_pagos_factura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPagosFacturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
